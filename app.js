8625ac4a0848f87e3826244fbe16c76507be800b
gouthm.04 <gouthm.04@gmail.com>
2026-07-14 23:43:59 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://goutham:goutham123@cluster0.umdwywd.mongodb.net/evstationdb")
+    .then(
+        () => {
+            console.log("MongoDB Connected")
+        }
+    ).catch(
+        (error) => {
+            console.log(error)
+        }
+    )
+
+const Booking = mongoose.model("Bookings", new mongoose.Schema(
+    {
+        bookingId: String,
+        ownerName: String,
+        email: String,
+        phone: String,
+        vehicleRegistrationNumber: String,
+        vehicleBrand: String,
+        vehicleModel: String,
+        batteryCapacity: String,
+        connectorType: String,
+        chargingDate: String,
+        timeSlot: String,
+        estimatedUnits: String,
+        chargingBayNumber: String
+    }
+))
+app.get("/test", (req,res) => {
+    res.send("Hi")
+})
+
+app.listen(3000,() => {
+    console.log("Server Started")
+})
